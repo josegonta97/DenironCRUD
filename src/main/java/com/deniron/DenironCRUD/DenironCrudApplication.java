105782f9280cf01f14ed989e3907a82d43006157
josegonta97 <josegonta97@gmail.com>
2023-06-07 16:29:18 +0200

    Inicializando Spring backend CRUD para Deniron

---
+++ src/main/java/com/deniron/DenironCRUD/DenironCrudApplication.java
@@ -0,0 +1,13 @@
+package com.deniron.DenironCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenironCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenironCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deniron/DenironCRUD/DenironCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deniron.DenironCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenironCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
